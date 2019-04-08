b1ca652ad5ad8a0460da7b7b67c68e163a77e781
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-08 22:15:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,13 @@
-class MyValidator < ActiveModel::Validator
-  def validate(post)
-    if post.title.include? "Won't Believe"
-      return true
-    elsif post.title.include? "Secret"
-      return true
-    elsif post.title.include? "Guess"
-      return true
-    elsif post.title.include? "Top "
-      return true
-    else
-      return false
-    end
-  end
-end
-
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  @@terms = [/Won't Believe/, /Secret/, /Guess/, /Top \d*/]
+
+  def clickbait
+    if @@terms.none? {|term| term.match(title)}
+      errors.add(:title, "not clicky enough")
+  end
 end

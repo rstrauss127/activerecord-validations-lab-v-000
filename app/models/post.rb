b871b35fa0c0630852ae57dc894956ca53a7171c
Rebecca Strauss <rstrauss127@gmail.com>
2019-04-08 22:05:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class MyValidator < ActiveModel::Validator
       return true
     elsif post.title.include? "Guess"
       return true
-    elsif post.title.include? "Top [number]"
+    elsif post.title.include? "Top"
       return true
     else
       return false
